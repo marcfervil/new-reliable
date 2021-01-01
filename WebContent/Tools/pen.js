a52e8ac0eef954e0006a4dee7bbded3021ae577d
diamondrubix <diamondrubix@gmail.com>
2021-01-01 16:07:50 -0600

    folder organization

---
+++ WebContent/Tools/pen.js
@@ -0,0 +1,48 @@
+//is mightier than the sword
+class Pen extends Tool{
+
+    constructor(){
+        super("Pen", "images/pen.svg");
+    }
+
+
+    canvasDragStart(pos){
+        this.svgPath = new SVGPath(this.reliable.canvas, pos);
+
+    }
+
+    canvasDrag(pos){
+        this.svgPath.addPoint(new Vector2(pos.x, pos.y));
+    }
+
+    canvasDragEnd(){
+        this.svgPath.smoothify();
+        Action.commit(this.reliable, {
+            action: "Draw",
+            id: this.svgPath.id,
+            path: this.svgPath.pathData,
+        });   
+        this.svgPath.delete();
+
+    }
+}
+
+class Draw extends Action{
+
+    constructor(data){
+        super(data);
+    }
+
+    execute(reliable){
+        super.execute(reliable);
+        this.svgPath = new SVGPath(reliable.canvas, new Vector2());
+        this.svgPath.replacePath(this.data.path);
+        reliable.svgs.push(this.svgPath);
+    }
+
+    undo(){
+        this.reliable.svgs.splice(this.reliable.svgs.indexOf(this.svgPath), 1);
+        this.svgPath.delete();
+    }
+
+}
