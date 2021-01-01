1cd234f03fe509afb483a89f22b6ea49b162147e
Marc <mfervil1@depaul.edu>
2021-01-01 16:19:19 -0600

    RedPen Test

--- WebContent/Tools/pen.js
+++ WebContent/Tools/pen.js
@@ -21,6 +21,27 @@ class Pen extends Tool{
             action: "Draw",
             id: this.svgPath.id,
             path: this.svgPath.pathData,
+            color: "#AAB2C0"
+        });   
+        this.svgPath.delete();
+
+    }
+}
+
+class RedPen extends Pen{
+
+    canvasDragStart(pos){
+        this.svgPath = new SVGPath(this.reliable.canvas, pos);
+        this.svgPath.svg.style.stroke = "red";
+    }
+
+    canvasDragEnd(){
+        this.svgPath.smoothify();
+        Action.commit(this.reliable, {
+            action: "Draw",
+            id: this.svgPath.id,
+            path: this.svgPath.pathData,
+            color: "red"
         });   
         this.svgPath.delete();
 
@@ -37,6 +58,7 @@ class Draw extends Action{
         super.execute(reliable);
         this.svgPath = new SVGPath(reliable.canvas, new Vector2());
         this.svgPath.replacePath(this.data.path);
+        this.svgPath.svg.style.stroke = this.data.color; 
         reliable.svgs.push(this.svgPath);
     }
 
