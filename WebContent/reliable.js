3ae05964bc0b3391a591dab3989bd6dca72d79b5
diamondrubix <diamondrubix@gmail.com>
2020-12-31 23:05:28 -0600

    broadcast

--- WebContent/reliable.js
+++ WebContent/reliable.js
@@ -28,7 +28,7 @@ class Reliable {
     
     network(){
         window.addEventListener('message', actionData => {
-            console.log(actionData);
+            console.log("actionData");
             Action.commit(actionData);
         });
     }
