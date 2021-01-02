a2ea669537271daac8d885895a3b12d52ecd3ffd
Marc <mfervil1@depaul.edu>
2021-01-01 20:28:34 -0600

    Pen id fix

--- WebContent/Tools/pen.js
+++ WebContent/Tools/pen.js
@@ -61,7 +61,7 @@ class Draw extends Action{
 
     execute(reliable){
         super.execute(reliable);
-        this.svgPath = new SVGPath(reliable.canvas, new Vector2());
+        this.svgPath = new SVGPath(reliable.canvas, new Vector2(), this.data.id);
         this.svgPath.replacePath(this.data.path);
         this.svgPath.svg.style.stroke = this.data.color; 
         reliable.svgs.push(this.svgPath);
