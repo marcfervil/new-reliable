b88d49cdd7a65476cde190a903d6a1b86e60c1ae
diamondrubix <diamondrubix@gmail.com>
2021-01-01 21:19:04 -0600

    intersections

--- WebContent/Tools/erase.js
+++ WebContent/Tools/erase.js
@@ -17,11 +17,19 @@ class Eraser extends Tool{
         this.svgRect.updateLocation(new Vector2(pos.x, pos.y));
     }
 
+
     canvasDragEnd(){
+    
+        
+        let hits = this.reliable.canvas.getIntersectionList(this.svgRect.svg, null);
+
+        
+        let hitIds = [];
+        for(let hit of hits)if(hit.parentNode.id != "canvas")hitIds.push(hit.parentNode.id);
+        
+
+        //$(this.drawRect).remove();
         this.svgRect.delete();
-       
-
-
     }
 }
 
