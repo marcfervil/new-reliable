9b1d0ee5b9b40bc499732186844d3a1c1ece7e72
diamondrubix <diamondrubix@gmail.com>
2021-01-01 22:40:55 -0600

    tempo

--- WebContent/Svg/svgPointer.js
+++ WebContent/Svg/svgPointer.js
@@ -5,17 +5,18 @@ class SVGPointer extends SVG{
 
         this.svg.style.stroke = "#AAB2C0"; 
         //this.svg.style.fill = "orange";
+        this.size = "30";
         this.svg.setAttributeNS(null, 'x', pos.x);
         this.svg.setAttributeNS(null, 'y', pos.y);
-        this.svg.setAttributeNS(null, 'height', '30');
-        this.svg.setAttributeNS(null, 'width', '30');
+        this.svg.setAttributeNS(null, 'height', this.size);
+        this.svg.setAttributeNS(null, 'width', this.size);
         this.svg.setAttributeNS(null, 'fill', '#'+Math.round(0xffffff * Math.random()).toString(16));
         //document.getElementById('svgOne').appendChild(rect);
 
     }
 
     updateLocation(pos){
-        let loc = "'translate("+pos.x+","+pos.y+"')"
+        this.pos = pos;
         this.svg.setAttributeNS(null, 'x', pos.x);
         this.svg.setAttributeNS(null, 'y', pos.y);
         //this.svg.setAttribute("transform", "translate(100,100)");
--- WebContent/Tools/erase.js
+++ WebContent/Tools/erase.js
@@ -9,27 +9,76 @@ class Eraser extends Tool{
 
     canvasDragStart(pos){
         this.svgRect = new SVGPointer(this.reliable.canvas, pos)
-        //this.svgPath = new SVGPath(this.reliable.canvas, pos);
 
     }
 
     canvasDrag(pos){
+        this.erase();
         this.svgRect.updateLocation(new Vector2(pos.x, pos.y));
     }
 
 
     canvasDragEnd(){
     
+        this.erase();
+        this.svgRect.delete();
+    }
+    checkCollisions(){
+        let svgRect = this.reliable.canvas.createSVGRect();
+
+        svgRect.x = this.svgRect.pos.x;
+        svgRect.y = this.svgRect.pos.y;
+        svgRect.width = this.svgRect.size;
+        svgRect.height = this.svgRect.size;
         
-        let hits = this.reliable.canvas.getIntersectionList(this.svgRect.svg, null);
+        let hits = this.reliable.canvas.getIntersectionList(svgRect, null);
+        let hitIds = [];
+        for(let hit of hits)if(hit.parentNode.id != this.svgRect.id && hit.parentNode.id != "canvas")hitIds.push(hit.parentNode.id);
+        console.log(hitIds);
+        return hitIds;
+    }
+    
+    eraseOverlap(test){
+        console.log(test)
+    }
+    erase(){
+ 
+        eraseOverlap(checkCollisions());
+
+    }
+
+
+
+
+
+    /*
+    canvasDragEnd(){
+      
+        
+        let rect = this.drawRect.getBoundingClientRect();
+        let svgRect = this.reliable.canvas.createSVGRect();
+
+        svgRect.x = rect.x;
+        svgRect.y = rect.y;
+        svgRect.width = rect.width;
+        svgRect.height = rect.height;
+        
+        let hits = this.reliable.canvas.getIntersectionList(svgRect, null);
 
         
         let hitIds = [];
         for(let hit of hits)if(hit.parentNode.id != "canvas")hitIds.push(hit.parentNode.id);
-        
 
-        //$(this.drawRect).remove();
-        this.svgRect.delete();
+
+        Action.commit(this.reliable, {
+            action: "Select",
+            ids: hitIds,
+        });   
+
+
+        $(this.drawRect).remove();
     }
+
+    */
 }
 
