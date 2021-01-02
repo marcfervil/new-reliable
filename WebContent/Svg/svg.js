d694e6492ed04477e3d6ef5d2c18a6516a7fded7
diamondrubix <diamondrubix@gmail.com>
2021-01-01 22:43:57 -0600

    Update svg.js

--- WebContent/Svg/svg.js
+++ WebContent/Svg/svg.js
@@ -1,5 +1,6 @@
 class SVG{
 
+
     constructor(type, parent, pos, id) {
         this.parent = parent;
         this.pos = pos;
@@ -9,13 +10,76 @@ class SVG{
         this.group = document.createElementNS("http://www.w3.org/2000/svg", 'g');
         this.svg = document.createElementNS("http://www.w3.org/2000/svg", this.type);
 
+        this.group.reliableSvg = this;
+        this.svg.reliableSvg = this;
+
         this.svg = this.group.appendChild(this.svg);
         this.group = this.parent.appendChild(this.group);
 
         this.group.setAttribute("id", this.id);
+
+        this.isSelected = false;
+        this.isDragging = false;
     }
 
+    selectedMouseMove(e){
+        console.log("dragging");
+    }
 
+    selectedMouseUp(e){
+        this.parent.removeEventListener('mousemove', this.mouseMoveRef);
+        this.parent.removeEventListener('mouseup', this.mouseUpRef);
+
+        console.log("done dragging");
+        this.isDragging = false; 
+    }
+
+    selectedMouseDown(e){
+        e.stopPropagation();
+        this.mouseMoveRef = (e) => this.selectedMouseMove(e);
+        this.mouseUpRef = (e) => this.selectedMouseUp(e);
+
+        this.parent.addEventListener('mousemove', this.mouseMoveRef);
+        this.parent.addEventListener('mouseup', this.mouseUpRef);
+
+        this.isDragging = true; 
+
+        console.log("Start drag");
+    }
+
+    createSelectRect(){
+        let bounds = this.group.getBoundingClientRect();
+        let selectRect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+        
+        this.group.appendChild(selectRect);
+        let margin = 10;
+
+        selectRect.setAttribute('x', bounds.x - margin);
+        selectRect.setAttribute('y', bounds.y - margin);
+        selectRect.setAttribute('width', bounds.width + (margin *2));
+        selectRect.setAttribute('height', bounds.height + (margin *2));
+        selectRect.style.stroke = "green";
+        selectRect.style.fill = "transparent";
+
+
+
+        selectRect.addEventListener('mousedown', (e) => this.selectedMouseDown(e));
+        
+
+
+        return selectRect;
+    }
+
+    select(mySelection){
+        if(this.isSelected) return;
+        this.isSelected = true;
+        this.selectRect = this.createSelectRect();
+    }
+
+    unselect(){
+        this.isSelected = false;
+        $(this.selectRect).remove();
+    }
 
     delete() {
         $(this.group).remove();
