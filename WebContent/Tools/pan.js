bb9b9d7e497b12dc6d2d174dc55c93909a418c28
Marc <mfervil1@depaul.edu>
2021-01-12 13:21:46 -0600

    G to place rect on mouse pos

--- WebContent/Tools/pan.js
+++ WebContent/Tools/pan.js
@@ -1,9 +1,25 @@
+
+
 class Pan extends Tool{
+
+
+
+    debugRect(x, y, w, h, color){
+        let debug = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+        debug.setAttribute("x", x);
+        debug.setAttribute("y", y);
+        debug.setAttribute("width", w);
+        debug.setAttribute("height", h);
+        debug.style.stroke = color;
+        debug.style.fill = "transparent";
+        canvas.appendChild(debug);
+        return debug;
+    }
 
     constructor(){
         super("Pan");
         this.scolled = 0;
-   
+        this.panVal = new Vector2(0, 0);
         window.onwheel = (e)=> {
             
             if(this.scolled ==0 )this.lastTool = this.reliable.getCurrentTool();
@@ -19,10 +35,66 @@ class Pan extends Tool{
                     this.reliable.swapTool(this.lastTool);
                     
                 }
-            }, 100);
-          
-            
+            }, 200);
+
         };
+
+
+
+        document.addEventListener('keydown', (event)=> {
+            if(event.key === 'f'){
+  
+                //zoom = new Vector2(0.5, 0.5);
+                //canvas.setAttribute("viewBox", `0 0 ${canvas.clientWidth*zoom.x} ${canvas.clientHeight*zoom.y}`);
+
+
+                //let deltaX = -parseInt(canvas.style.left.substr(0, canvas.style.left.length-2)) ;
+                //let deltaY = -parseInt(canvas.style.top.substr(0, canvas.style.top.length-2)) 
+          
+                //canvas.setAttribute("viewBox", `${deltaX} ${deltaY} ${canvas.clientWidth*0.5} ${canvas.clientHeight*0.5}`);
+                
+                //
+
+                //let offset = new Vector2(-(canvas.clientWidth*zoom.x)/2, -(canvas.clientHeight*zoom.y)/2);
+                let cx = parseInt(canvas.style.left.substr(0, canvas.style.left.length-2)) ;
+                let cy = parseInt(canvas.style.top.substr(0, canvas.style.top.length-2)) ;
+                let canvasOffset = new Vector2(-cx, -cy);
+                let canvasBounds = canvas.getBoundingClientRectOld();
+
+                //let dist = mousePos.subtract(new Vector2((screen.width/2),(screen.height/2).add()));
+                //console.log(canvas.clienrtX+(canvas.clientWidth));
+
+                let w = document.body.clientWidth;
+                let h = document.documentElement.clientHeight;
+
+                let center = new Vector2((w/2)-cx, (h/2)-cy);
+
+                let mouseOffset = mousePos.subtract(new Vector2(cx, cy)).subtract(center);
+
+                let centerMouse = center.add(mouseOffset);
+                this.debugRect(centerMouse.x, centerMouse.y, 10, 10, "red");
+
+              //  console.log(dist);
+                //this.pan(dist, true);
+
+
+                /*
+                let deltaX = parseInt(canvas.style.left.substr(0, canvas.style.left.length-2)) ;
+                let deltaY = parseInt(canvas.style.top.substr(0, canvas.style.top.length-2)) ;
+
+                let panOffset = new Vector2(deltaX, deltaY);
+                let offset = mousePos.add(panOffset);
+
+                let deltaPan = offset.subtract(mousePos);
+
+                this.pan(offset, true);
+                console.log(deltaPan);
+                this.pan(deltaPan)*/
+    
+            }
+    
+        });
+
     }
 
     getImage(){
@@ -34,9 +106,18 @@ class Pan extends Tool{
 
     }
 
-    pan(delta){
-        $(canvas).css( 'left', `+=${delta.x}px` );
-        $(canvas).css( 'top', `+=${delta.y}px` );
+    pan(delta, hard){
+        /*
+        pan = pan.subtract(delta);
+        canvas.setAttribute("viewBox", `${pan.x} ${pan.y} ${canvas.clientWidth} ${canvas.clientHeight}`);*/
+        
+        if(hard==true){
+            $(canvas).css( 'left', `${delta.x}px` );
+            $(canvas).css( 'top', `${delta.y}px` );
+        }else{
+            $(canvas).css( 'left', `+=${delta.x}px` );
+            $(canvas).css( 'top', `+=${delta.y}px` );
+        } 
     }
 
     canvasDrag(pos){
