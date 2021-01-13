7a88f1bc0b4face28952caa7998613e92248478c
Marc <mfervil1@depaul.edu>
2021-01-13 15:00:55 -0600

    double click to type

--- WebContent/Tools/text.js
+++ WebContent/Tools/text.js
@@ -49,15 +49,20 @@ class TextInput{
         let objSVG = document.createElementNS("http://www.w3.org/2000/svg", 'foreignObject');
         objSVG.setAttribute("x", this.pos.x);
         objSVG.setAttribute("y", this.pos.y);
-        objSVG.setAttribute("width", 200+"px");
+        objSVG.setAttribute("width", "10000px");
         objSVG.setAttribute("height", 50+"px");
+        objSVG.style.overflow="hidden";
 
-        
-        objSVG.style.zIndex = 1000;
         let objDiv = document.createElement('div');
-        //objDiv.setAttribute("xmlns", "http://www.w3.org/1999/xhtml");
-        let text = document.createTextNode("yuh");
-        objDiv.appendChild(text);
+        
+        let text = $("<input/>").attr("type", "text").attr("class", "SVGInput");
+        $(objDiv).append(text);
+        setTimeout(()=>{
+           
+            text.focus();
+            window.scrollTo(0, 0);
+        },0);
+
         objSVG.appendChild(objDiv);
  
         this.reliable.canvas.appendChild(objSVG);
