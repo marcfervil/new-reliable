a5ea5504c35503ed6643789a1da2a46f70f67b1a
Marc <mfervil1@depaul.edu>
2021-01-17 14:00:42 -0600

    Basic Socket.io config

--- WebContent/platform.js
+++ WebContent/platform.js
@@ -10,3 +10,8 @@ class Platform{
 
 
 }
+
+
+var socket = io();
+
+//console.log("???");
--- index.js
+++ index.js
@@ -3,32 +3,32 @@ const handlebars = require("handlebars");
 const path =require('path');
 const app = express();
 var fs = require('fs');
-const port = 3005
-
+const port = 3005;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 /*
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })*/
 
+app.use("/static",express.static(path.join(__dirname, 'WebContent')));
 
-
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     let indexPath = path.join(__dirname, 'WebContent', "index.html");
     var html = fs.readFileSync(indexPath, 'utf8');
     html = handlebars.compile(html)({path: "/static"}) ;
     res.send(html);
+    
 });
 
-app.get('/static/platform.js', (req, res) => {
-    let indexPath = path.join(__dirname, 'WebContent', "platformWeb.js");
-    var html = fs.readFileSync(indexPath, 'utf8');
-    res.send(html);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    console.log("Fewijfwei???")
 });
+  
 
-app.use("/static",express.static(path.join(__dirname, 'WebContent')));
-
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+http.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
